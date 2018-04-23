ff128dd829a6e9d534ff7afd98d3ab3cf5c111ad
eko0427 <nakata.keisuke123@gmail.com>
2018-04-24 00:58:54 +0900

    関数化などの修正

--- othello.py
+++ othello.py
@@ -6,108 +6,88 @@
 # 番号      更新履歴                                    日付        氏名
 # -----------------------------------------------------------------------------
 # 000000    新規作成                                    2018/04/18  中田　桂介
+# 000001    関数化                                      2018/04/22  中田　桂介
+# 000002    input時のValueError回避                     2018/04/23  中田　桂介
+# 000003    定数の宣言、終了判定の変更、プレイヤーの入れ替えの変更
+#                                                       2018/04/23　中田　桂介
 # -----------------------------------------------------------------------------
-flag = 0
-while flag == 0:                                 #プログラム全体のループ
-# 初期化 ----------------------------------------------------------------------
-    map = [2]*15                                #マップの初期化
-    player = 0                                  #手番プレイヤーのコマ
-    next_player = 1                             #次の手番プレイヤーのコマ
-    pieces = ['●', '〇']                        #コマ
-    val = 0
-    white_piece = 0                             #白のコマの数
-    black_piece = 0                             #黒のコマの数
-# 1ゲーム開始 ------------------------------------------------------------------
-    while 1:                                    #1ゲーム全体用のループ
+
 # 表示部 ----------------------------------------------------------------------
-        print('  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15')
-        print('----------------------------------------------')
-        for loop_count in range(15):            #コマ表示用に15回ループ
-            if map[loop_count] == 2:            #コマがあるかどうか判定
-                print('|  ',end='')
-            elif map[loop_count] == 0:          #コマが白かどうかを判定
-                print('|● ',end='')
-            elif map[loop_count] == 1:
-                print('|〇',end='')             #コマが黒かどうかを判定
-        print('|')
-        print('----------------------------------------------')
-# 入力部 ----------------------------------------------------------------------
-        print(pieces[player] + "さんの番です。何処にコマを置きますか？")
-        while 1:                                #入力ミスした時用のループ
-            input_pointer = input('→')          #数値を入力してもらう
-            pointer = int(input_pointer) - 1
-            if pointer >= 0 and pointer <= 14:  #入力された値がマップ内か判定
-                if map[pointer] == 2:           #入力された場所が空白か判定
-                    map[pointer] = player       #空白だった場合コマを配置
-                    break
-                elif map[pointer] == 0 or map[pointer] == 1:#空白以外だった場合の判定
-                    print("既に" + pieces[player] + 'が配置されています。再入力して下さい。')
-            else:                               #数値以外を入力した場合下記を表示する
-                print("入力にミスがありました。再入力して下さい。")
-# 左の石の状況を調べ、ひっくり返す ----------------------------------------------
-        if pointer - 1 >= 0:                    #マップ外に出ていないかを判定
-            if map[pointer - 1] == next_player: #左の石が相手のコマを判定
-                check = pointer - 2             #今どこにいるのかのポインタ
-                while 1:                        #複数個ひっくり返すためのループ
-                    if check < 0:               #ポインタがマップ外に出ていないか判定
-                        break
-                    if map[check] == player:    #次に自分が来たかを判定
-                        while map[check + 1] == next_player:#挟んだコマをひっくり返すまで回す
-                            map[check + 1] = player#ポインタから一つ右のコマをひっくり返す
-                            check += 1          #ポインタを進める
-                        break
-                    elif map[check] == next_player:#次に相手のコマが来たか判定
-                        check -=1               #ポインタを進める
-                    else:                       #それ以外だった場合ループをやめる
-                        break
-# 右の石の状況を調べ、ひっくり返す ----------------------------------------------
-        if pointer + 1 < 14:                    #マップ外に出ていないかを判定
-            if map[pointer + 1] == next_player: #右の石が相手のコマを判定
-                check = pointer + 2             #今どこにいるのかのポインタ
-                while 1:                        #複数個ひっくり返すためのループ
-                    if check > 14:              #ポインタがマップ外に出ていないか判定
-                        break
-                    if map[check] == player:    #次に自分が来たかを判定
-                        while map[check - 1] == next_player:#挟んだコマをひっくり返すまで回す
-                            map[check - 1] = player#ポインタから一つ左のコマをひっくり返す
-                            check -= 1          #ポインタを進める
-                        break
-                    elif map[check] == next_player:#次に相手のコマが来たか判定
-                        check +=1               #ポインタを進める
-                    else:                       #それ以外だった場合ループをやめる
-                        break
-# プレイヤーの手番変更 ---------------------------------------------------------
-        val = player
-        player = next_player
-        next_player = val
-# ゲーム終了するかの判定 -------------------------------------------------------
-        white_piece = 0                         #初期化
-        black_piece = 0
-        for loop_count in range(15):            #15ループしてコマの数を数える
-            if map[loop_count] == 0:            #白のコマの数を数える
-                white_piece += 1
-            elif map[loop_count] == 1:          #黒のコマの数を数える
-                black_piece += 1
-        if (white_piece + black_piece) >= 15:   #コマの合計が15以上なら終了
-            break
-# 表示部 ----------------------------------------------------------------------
+def othello_display(map):
     print('  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15')
     print('----------------------------------------------')
-    for loop_count in range(15):
-        if map[loop_count] == 2:
+    for loop_count in range(15):            #コマ表示用に15回ループ
+        if map[loop_count] == 0:            #コマがあるかどうか判定
             print('|  ',end='')
-        elif map[loop_count] == 0:
+        elif map[loop_count] == 1:         #コマが白かどうかを判定
             print('|● ',end='')
-        elif map[loop_count] == 1:
-            print('|〇',end='')
+        elif map[loop_count] == -1:
+            print('|〇',end='')             #コマが黒かどうかを判定
     print('|')
     print('----------------------------------------------')
-    if (white_piece + black_piece) >= 15:       #コマの合計が15個以上か判定
-        if white_piece > black_piece:           #どちらのコマが多いか判定
-            print("●の勝ち！")
-        else:
-            print("〇の勝ち！")
+# 入力部 ----------------------------------------------------------------------
+def othello_input(map, PIECES, player, game_flag):
+    print(PIECES[player + 1] + "さんの番です。何処にコマを置きますか？")
+    while 1:                                #入力ミスした時用のループ
+        input_pointer = input('→')          #数値を入力してもらう
+        try:
+            pointer = int(input_pointer) - 1
+            if pointer >= 0 and pointer <= 14:#入力された値がマップ内か判定
+                if map[pointer] == 0:       #入力された場所が空白か判定
+                    map[pointer] = player   #空白だった場合コマを配置
+                    game_flag += player     #勝敗付のためにプレイヤーによって+か-する
+                    break
+                elif map[pointer] == -1 or map[pointer] == 1:#空白以外だった場合の判定
+                    print("既に" + PIECES[player + 1] + 'が配置されています。再入力して下さい。')
+            else:                           #数値以外を入力した場合下記を表示する
+                print("入力にミスがありました。再入力して下さい。")
+        except ValueError:
+            print("入力にミスがありました。再入力して下さい。")
+    return map, pointer, game_flag
+# 左の石の状況を調べ、ひっくり返す ----------------------------------------------
+def othello_overturn(map, pointer, player, next_player, game_flag):
+    if pointer - 1 >= 0:                    #マップ外に出ていないかを判定
+        if map[pointer - 1] == next_player: #左の石が相手のコマを判定
+            check = pointer - 2             #今どこにいるのかのポインタ
+            while 1:                        #複数個ひっくり返すためのループ
+                if check < 0:               #ポインタがマップ外に出ていないか判定
+                    break
+                if map[check] == player:    #次に自分が来たかを判定
+                    while map[check + 1] == next_player:#挟んだコマをひっくり返すまで回す
+                        map[check + 1] = player#ポインタから一つ右のコマをひっくり返す
+                        game_flag += player #勝敗付のためにプレイヤーによって+か-する
+                        check += 1          #ポインタを進める
+                    break
+                elif map[check] == next_player:#次に相手のコマが来たか判定
+                    check -=1               #ポインタを進める
+                else:                       #それ以外だった場合ループをやめる
+                    break
+# 右の石の状況を調べ、ひっくり返す ----------------------------------------------
+    if pointer + 1 < 14:                    #マップ外に出ていないかを判定
+        if map[pointer + 1] == next_player: #右の石が相手のコマを判定
+            check = pointer + 2             #今どこにいるのかのポインタ
+            while 1:                        #複数個ひっくり返すためのループ
+                if check > 14:              #ポインタがマップ外に出ていないか判定
+                    break
+                if map[check] == player:    #次に自分が来たかを判定
+                    while map[check - 1] == next_player:#挟んだコマをひっくり返すまで回す
+                        map[check - 1] = player#ポインタから一つ左のコマをひっくり返す
+                        game_flag += player #勝敗付のためにプレイヤーによって+か-する
+                        check -= 1          #ポインタを進める
+                    break
+                elif map[check] == next_player:#次に相手のコマが来たか判定
+                    check +=1               #ポインタを進める
+                else:                       #それ以外だった場合ループをやめる
+                    break
+    return map, game_flag
+# プレイヤーの手番変更 ---------------------------------------------------------
+def player_change(player, next_player):
+    val = player
+    player = next_player
+    next_player = val
+    return player , next_player
 # ゲームを続けるのか判定 -------------------------------------------------------
+def othello_choice(flag):
     print("ゲームを続けますか？")
     print("Y : YES / N : NO")
     while 1:                                    #入力ミスした時用のループ
@@ -119,6 +99,35 @@ while flag == 0:                                 #プログラム全体のルー
             break
         else:                                   #入力ミスした場合下記を表示
             print("入力にミスがありました。再入力して下さい。")
+    return flag
+
+if __name__ == "__main__":
+# 定数の宣言 ------------------------------------------------------------------
+    PIECES = ['〇','　', '●']                       #コマ表示用のリスト
+    MAP_RESET = [0]*15                              #map初期化用の定数
+# ゲームスタート --------------------------------------------------------------
+    flag = 0
+    while flag == 0:                                #プログラム全体のループ
+# 初期化 ----------------------------------------------------------------------
+        map = MAP_RESET                             #マップの初期化
+        player = 1                                  #手番プレイヤーのコマ
+        next_player = -1                            #次の手番プレイヤーのコマ
+        pointer = 0                                 #探索中の自分の場所
+        game_flag = 0                               #勝敗用のフラグ
+# 1ゲーム開始 ------------------------------------------------------------------
+        for loop_count in range(15):                #1ゲーム全体用のループ
+            othello_display(map)
+            map, pointer, game_flag = othello_input(map, PIECES, player, game_flag)
+            map, game_flag = othello_overturn(map, pointer, player, next_player, game_flag)
+# プレイヤーの手番変更 ---------------------------------------------------------
+            player, next_player = player_change(player, next_player)
+# 表示部 ----------------------------------------------------------------------
+        othello_display(map)
+        if game_flag >= 1:                          #game_flagの数値によって勝敗の判定
+            print("●の勝ち！")
+        else:
+            print("〇の勝ち！")
+        flag = othello_choice(flag)
 # ----------------------------------------------------------------------------
 #              Copyright HAL College of Technology & Design
 # ----------------------------------------------------------------------------
